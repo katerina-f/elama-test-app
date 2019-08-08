9559ad38235a78375d853a0342b43213d1cea481
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-08 12:56:41 +0300

    Добавлена модель User

---
+++ test_app/user/models.py
@@ -0,0 +1,11 @@
+from test_app.extensions import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return '<User id:{}, email:{}>\n'.format(self.id, self.email)
