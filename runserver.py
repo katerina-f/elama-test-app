93600a5368760bba6e272ac8392ac095f078d1e1
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-12 18:43:40 +0300

    Добавлен запуск поиска пользователей (для будущей рассылки уведомлений)

--- runserver.py
+++ runserver.py
@@ -1,1 +1,7 @@
 from test_app.app import app
+from test_app.extensions import db
+from test_app.user.models import User
+from test_app.reminder.reminder import run_reminder
+
+
+run_reminder(3, db, User)
