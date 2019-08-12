0a5288140c1da2ef0bbdf2fb6c414da383a69493
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-12 18:42:02 +0300

    Добавлено поле дня рождения в модель пользователя

--- test_app/user/models.py
+++ test_app/user/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+    birth_date= db.Column(db.Date, nullable=True)
 
     def __repr__(self):
         return '<User id:{}>\n'.format(self.id)
