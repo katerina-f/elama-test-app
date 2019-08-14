3c28d48557e17c97043189b9c7d82c97f231c0ef
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-14 18:19:20 +0300

    Добавлено логгирование ошибок в роутах и предупреждения в лог на функцию добавления пользователя

--- test_app/routes.py
+++ test_app/routes.py
@@ -1,11 +1,14 @@
 from flask import request
 from flask import render_template
 from flask import jsonify
+
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.exc import DataError
 from sqlalchemy.exc import IntegrityError
 
 from test_app.app import app
+from test_app.app import logger
+
 from test_app.user.models import User
 
 from test_app.reminder.reminder import BdayNotificator
@@ -14,11 +17,13 @@ from test_app.extensions import db
 
 
 @app.route('/')
+@logger.catch(level='ERROR')
 def ping():
     return 'OK', 200
 
 
 @app.route('/users')
+@logger.catch(level='ERROR')
 def get_user():
     """
     Отображает всех пользователей, находящихся в базе
@@ -29,30 +34,35 @@ def get_user():
 
 
 @app.route('/add_user', methods=['GET'])
+@logger.catch(level='ERROR')
 def add_user():
     return render_template('user.html')
 
 
 @app.route('/added_user', methods=['POST'])
+@logger.catch(level='ERROR')
 def added_user():
     user = User(first_name=request.form['First name'], last_name=request.form['Last name'], birth_date=request.form['birthday'])
     db.session.add(user)
     try:
         db.session.commit()
     except OperationalError as ex:
-        db.session.rollback()
+        logger.warning('Произошла ошибка подключения к базе данных')
         return jsonify({'error': "Bad connection. Try again later."})
     except DataError as ex:
         db.session.rollback()
+        logger.warning('Были введены неверные данные')
         return jsonify({'error': "Data Format Error"})
     except IntegrityError as ex:
         db.session.rollback()
+        logger.warning('Была попытка добавить существующего пользователя')
         return jsonify({'error': "User already exists"})
 
     return 'USER {} {} added successfully'.format(user.first_name, user.last_name)
 
 
 @app.route('/get_birthdays', methods=['GET'])
+@logger.catch(level='ERROR')
 def create_notification():
     notificator = BdayNotificator(db, User, interval=(0, 1))
     data = notificator.bd_prompt()
