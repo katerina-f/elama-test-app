780454cf787358d871e802673d92e2dd11146680
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-14 16:04:30 +0300

    ISSUE-5, ISSUE-6, ISSUE-3 Изменена модель пользователя, убрана авторизация

--- test_app/user/models.py
+++ test_app/user/models.py
@@ -4,8 +4,8 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    first_name = db.Column(db.String(120))
+    last_name = db.Column(db.String(128))
     birth_date= db.Column(db.Date, nullable=True)
 
     def __repr__(self):
