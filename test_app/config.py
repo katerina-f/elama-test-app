705eaf388dc1ac80a42647c4395b834ab8727eae
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-16 17:25:25 +0300

    Добавлена конфиги для почты

--- test_app/config.py
+++ test_app/config.py
@@ -11,8 +11,17 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
         'test', '1234', 'localhost', 'postgres')
 
+    MAIL_SERVER = 'smtp.yandex.ru'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    INTERVAL = (0,1)
 
 class DevelopmentConfig(Config):
     DEBUG = True
     LOG_BACKTRACE = True
     LOG_LEVEL = 'DEBUG'
+
+
