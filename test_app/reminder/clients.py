76bb51a85332ec3e20375025540c9163b717af06
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-20 18:07:32 +0300

    Изменена форма отправки сообщений

--- test_app/reminder/clients.py
+++ test_app/reminder/clients.py
@@ -1,17 +1,34 @@
 from abc import ABC
 from abc import abstractmethod
-import time
+import json
+from threading import Thread
+from datetime import datetime
 
 from flask_mail import Message
+from flask import render_template
+from test_app.app import app
+from test_app.app import mail
+from test_app.app import logger
 
 import pika
 
-"""Шаблоны для отправки в разные клиентыю
-Дальше предполагается подписка на очередь и обработка сообщений из нее"""
+"""Шаблоны для отправки в разные клиенты."""
 
 class AbstractClient(ABC):
     @abstractmethod
-    def update(self):
+    def update(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def callback(self, *args, **kwargs):
+        pass
+
+    def parsing_request(self, body):
+        msg_body = json.loads(body)
+        return msg_body
+
+    @abstractmethod
+    def subscribe_to_queue(self):
         pass
 
 
@@ -19,33 +36,44 @@ class EmailClient(AbstractClient):
     def __init__(self):
         pass
 
-    def parsing(self, connection):
+    @logger.catch(level='ERROR')
+    def update(self, message, recipients):
+        with app.app_context():
+            msg = Message('Birthdays', sender='katyfr0lova@yandex.ru', recipients=recipients)
+            msg.html = render_template('birthdays.html', users_list=message)
+            try:
+                mail.send(msg)
+            except:
+                print('something wrong', datetime.now())
+
+    def callback(self, ch, method, properties, body):
+        recipients = ['ekaterina.frolova40@gmail.com', 'katyfr0lova@yandex.ru']
+        msg_body = self.parsing_request(body)
+        thr = Thread(target=self.update, args=[msg_body, recipients])
+        thr.start()
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def subscribe_to_queue(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(
+            host='localhost'))
         channel = connection.channel()
 
-        channel.queue_declare(queue=self.__str__(), durable=True)
-        print(' [*] Waiting for messages. To exit press CTRL+C')
-
-        def callback(ch, method, properties, body):
-            print(body)
-            time.sleep(2)
-            print(" [x] Done")
-            ch.basic_ack(delivery_tag=method.delivery_tag)
+        channel.queue_declare(queue=__class__.__name__, durable=True)
 
         channel.basic_qos(prefetch_count=1)
-        channel.basic_consume(self.__str__(), callback, auto_ack=False)
-
-        channel.start_consuming()
-
-
-    def update(self):
-        pass
+        channel.basic_consume(__class__.__name__, self.callback, auto_ack=False)
+        try:
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            channel.stop_consuming()
+            connection.close()
 
 
 class SMSClient(AbstractClient):
     def __init__(self):
         pass
 
-    def parsing(self, data):
+    def callback(self, *args, **kwargs):
         pass
 
     def update(self):
@@ -56,7 +84,7 @@ class SlackClient(AbstractClient):
     def __init__(self):
         pass
 
-    def parsing(self, data, queue):
+    def callback(self, *args, **kwargs):
         pass
 
     def update(self):
