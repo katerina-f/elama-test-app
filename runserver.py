f205d6fe30a485c3e1da8a23834fae0429141e6f
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-20 18:03:48 +0300

    Добавлен запуск отправки уведомлений по графику

--- runserver.py
+++ runserver.py
@@ -1,5 +1,12 @@
 from test_app.app import app
 from test_app import routes
+import schedule
+import time
+from test_app.reminder.send_notifications import send_notifications
 
+schedule.every(1).minutes.do(send_notifications)
 
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
 
