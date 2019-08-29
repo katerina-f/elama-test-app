6fb7d52c4882da890332144d0c52d89fa793d354
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-08-29 13:55:55 +0300

    Изменен запуск скрипта

--- runserver.py
+++ runserver.py
@@ -1,12 +1,21 @@
-from test_app.app import app
-from test_app import routes
-import schedule
 import time
+import schedule
+
+from test_app.app import app
 from test_app.reminder.send_notifications import send_notifications
+from test_app.app import logger
 
-schedule.every(1).minutes.do(send_notifications)
 
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+def main():
+    try:
+        schedule.every(2).minutes.do(send_notifications)
 
+        while 1:
+            schedule.run_pending()
+            time.sleep(1)
+    except Exception as ex:
+        logger.warning(ex.__str__())
+
+if __name__ == '__main__':
+    with app.app_context():
+        main()
